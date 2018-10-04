c69c02db79a68c4ba2bfd1ec565288f33d4b34e6
Catherine Bronzo <cbronzo@mac.com>
2018-10-04 21:29:58 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,9 @@ class SessionsController < ApplicationController
   def create
   end
   
-  def destroy
-  end
+  def destroy 
+    session.delete :name
+    redirect_to sessions_new_path
+  end 
   
 end
